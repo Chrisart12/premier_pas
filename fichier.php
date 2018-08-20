05ee8cfea387272b59c686841031254151f9988c
Chris <chrisissifou@gmail.com>
2018-08-20 16:55:02 +0200

    Vérification du droit de jouer

--- fichier.php
+++ fichier.php
@@ -3,3 +3,12 @@
 $mon_age = 40;
 
 echo $mon_age;
+
+if ($mon_age >= 18)
+{
+	echo "Le visiteur est autorisé à jouer, il a " . $mon_age . " ans."
+}
+else
+{
+	echo "Le visiteur n'est pas autorisé à jouer, il a moins de 18 ans.";
+}
